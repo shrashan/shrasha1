284525a38db0acc24ef2af178f0d0b44ae492eb5
shrashan <55472267+shrashan@users.noreply.github.com>
2019-09-20 13:30:18 -0500

    adding a new project to github

---
+++ Tutorial_17_constructors/src/Constructorss.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Robot{
+	private String name;
+	private int memory;
+	
+	public Robot() {
+		this("ram",45);
+		
+		System.out.println("The name of the Robot is "+name+" and its memory is "+memory+" GB");
+		name="Rashan";
+				memory=32;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getMemory() {
+		return memory;
+	}
+	
+	public Robot(String name, int memory) {
+		this.name=name;
+		this.memory=memory;
+		
+	}
+
+public String getNamee(){
+	return name;
+}
+public int getMeemory() {
+	return memory;
+}
+
+	
+}
+public class Constructorss {
+	
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		
+		Robot robo1=new Robot();
+		System.out.println("The name of the robot is "+robo1.getName());
+		System.out.println("The memory of the robot is "+robo1.getMemory());
+		
+		Robot robo2=new Robot("sam",64);
+		System.out.println("The name of the robot is "+robo2.getNamee());
+		System.out.println("The memory of the robot is "+robo2.getMeemory());
+		
+		
+		
+	}
+
+}
